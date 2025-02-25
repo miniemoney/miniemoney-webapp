8f970e28b2cad3730ec464bdf3cafb0f5d77f8eb
hammedt20 <hammedt20@gmail.com>
2025-02-25 05:03:55 -0800

    added seo to the pages

---
+++ src/seoData.tsx
@@ -0,0 +1,16 @@
+
+export const seoData: any = {
+  "/": {
+    title: "Minie money",
+    description: "Receive money easily with a personalized link.",
+    image: "",
+    url: "https://miniemoney.com",
+  },
+  "/gifting": {
+    title: "Minie money || Gifting",
+    description:
+      "Support your loved ones’ savings with a thoughtful cash gift.",
+    image: "",
+    url: "https://miniemoney.com/gifting",
+  },
+};
